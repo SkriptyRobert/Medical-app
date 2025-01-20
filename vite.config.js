f0272df2fe22af8064ed86341721e8460d6bc79b
Pešout Róbert <107212437+SkriptyRobert@users.noreply.github.com>
2025-01-20 09:43:54 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,13 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    sourcemap: true
+    sourcemap: true,
+    commonjsOptions: {
+      transformMixedEsModules: true
+    }
+  },
+  optimizeDeps: {
+    include: ['@react-three/fiber', '@react-three/drei']
   },
   assetsInclude: ['**/*.gltf', '**/*.glb', '**/*.bin'],
   server: {
